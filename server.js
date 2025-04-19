cf2429ec348b45164e7564881b1422a67e31cd88
sebasramirez830 <sebasramirez830@gmail.com>
2025-04-19 09:38:58 -0500

    En la carpeta server.js se agrego la manera de visualizar la pagina por medio de un servidor local

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// Servidor básico para levantar la app
+// Servidor básico para ejecutar la aplicación
+
+/*
+Formas de visualizar la pagina web:
+
+1) A traves de GitHub Pages:
+Se ha desplegado la aplicacion en GitHub Pages. Puedes acceder mediante el siguiente enlace:
+🔗 https://sebas830.github.io/ProyectoFinalCompiladores/
+
+2) A traves de un servidor local usando Node.js:
+
+Pasos:
+
+- Asegurate de tener Node.js instalado.
+- Abre Visual Studio Code y carga la carpeta del proyecto: Compiladores_taller_final.
+- Presiona Ctrl + J para abrir la terminal integrada.
+- En la terminal, ejecuta el siguiente comando para instalar el servidor local:
+    npm install -g serve
+- Luego, ejecuta el siguiente comando para iniciar el servidor:
+    serve .
+- Presiona Ctrl + clic en el enlace que aparece en la terminal (por defecto será: http://localhost:3000)
+*/
+
