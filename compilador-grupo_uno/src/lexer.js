c0393ac4f0cb4c978ea033c116e1337c05dbe86b
sebasramirez830 <sebasramirez830@gmail.com>
2025-04-19 01:04:33 -0500

    Se agregaron comentarios al lexer.js, pendiente mejorar el semantic

--- compilador-grupo_uno/src/lexer.js
+++ compilador-grupo_uno/src/lexer.js
@@ -2,23 +2,27 @@ const palabrasReservadas = new Set(["int", "float", "if", "while", "return", "st
 const operadores = new Set(["+", "-", "*", "/", "=", ">", "<", "==","!","?","&&","||"]);
 const delimitadores = new Set([";", "{", "}", "(", ")"]);
 
+//analiza el codigo y define sus tokens tipo de tokens
 function analizarCodigo(codigo) {
-    const patronTokens = /(".*?"|\b\d+\.\d+\b|\b\d+\b|\b[a-zA-Z_]\w*\b|[+\-*/=<>!]=?|[;{}()])/g;
-    let coincidencias = [...codigo.matchAll(patronTokens)];
-    let listaTokens = [];
+    const patronTokens = /(".*?"|\b\d+\.\d+\b|\b\d+\b|\b[a-zA-Z_]\w*\b|[+\-*/=<>!]=?|[;{}()])/g; // expresion regular
+    let coincidencias = [...codigo.matchAll(patronTokens)]; //extracion de tojens
+    let listaTokens = []; // lista los tokens
     let tablaSimbolos = {};
     let numeroLinea = 1;
     let tipoActual = null;
     let posicionActual = 0;
 
+    // Coinsidencia de tokens detectados
     for (let coincidencia of coincidencias) {
         let token = coincidencia[0];
         let tipoToken;
 
+        // Validar saltos de linea
         let antesDelToken = codigo.substring(posicionActual, coincidencia.index);
         numeroLinea += (antesDelToken.match(/\n/g) || []).length;
         posicionActual = coincidencia.index + token.length;
 
+        //Definir los tokens
         if (palabrasReservadas.has(token)) {
             tipoToken = 'PALABRA_RESERVADA';
             if (["int", "float", "string"].includes(token)) {
@@ -50,25 +54,30 @@ function analizarCodigo(codigo) {
             }
         }
 
-        listaTokens.push({ token, tipoToken, numeroLinea });
+        listaTokens.push({ token, tipoToken, numeroLinea });// Rgregar token analizado a la lista de tokens
     }
 
-    return { listaTokens, tablaSimbolos };
+    return { listaTokens, tablaSimbolos }; // Retornamos las tablas
 }
 
+//Esta funcion se ejecuta cuando el usuario haga click en analisis lexico
 function analizarLexico() {
-    const codigo = document.getElementById("codigo").value;
-    const { listaTokens, tablaSimbolos } = analizarCodigo(codigo);
+    const codigo = document.getElementById("codigo").value; //Obtenemos la informacion ingresada por el usuario
+    const { listaTokens, tablaSimbolos } = analizarCodigo(codigo);// Usamos la funcion analizar codigo
 
+    // Limpia la tabla de tokens
     const tablaTokensBody = document.getElementById("tablaTokens").querySelector("tbody");
     tablaTokensBody.innerHTML = "";
     listaTokens.forEach(t => {
+        //Mostrar tabla de tokens en el html
         const fila = `<tr><td>${t.token}</td><td>${t.tipoToken}</td><td>${t.numeroLinea}</td></tr>`;
         tablaTokensBody.innerHTML += fila;
     });
 
+    // Limpia la tablaSimbolos
     const tablaSimbolosBody = document.getElementById("tablaSimbolos").querySelector("tbody");
     tablaSimbolosBody.innerHTML = "";
+    // Recorre la tabla de simbolos y muestra los identificadores
     Object.entries(tablaSimbolos).forEach(([nombre, info]) => {
         const fila = `<tr><td>${nombre}</td><td>${info.Tipo}</td><td>${info.Token}</td><td>${info.Lineas.join(", ")}</td></tr>`;
         tablaSimbolosBody.innerHTML += fila;
